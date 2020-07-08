c51897e21b8aa26b6cfc7f1a5ab7f135b5fdd990
Joshi Saurabh Subhashrao <52069991+joshi2000-collab@users.noreply.github.com>
2020-07-08 14:54:21 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class mul {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+	    int t=s.nextInt();
+	    
+	    for(int i=1;i<=10;i++)
+	    {
+	    	System.out.println(t+"*"+i+"="+t*i);
+	    }
+		
+		
+	}
+
+}
